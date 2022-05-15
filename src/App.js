728eea3692d2077e3af801c45e2fdec0bf3f2c2b
abdulawalemon1 <emondx4@gmail.com>
2022-05-15 19:16:43 +0600

    components initialized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/Home/Home/About/About';
+import Home from './Pages/Home/Home/Home';
+import Navbar from './Pages/Home/Home/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
-
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
