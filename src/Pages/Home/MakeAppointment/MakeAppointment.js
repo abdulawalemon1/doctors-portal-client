80844d9460dcb0fd45154fb778d5f494827a86a7
abdulawalemon1 <emondx4@gmail.com>
2022-05-16 02:17:49 +0600

    testimonial section added

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -8,13 +8,13 @@ const MakeAppointment = () => {
         <section style={{
             background: `url(${bg})`
         }} className='flex justify-center items-center'>
-            <div className='flex-1'>
-                <img src={doctor} alt="" />
+            <div className='flex-1 hidden lg:block'>
+                <img className='lg:mt-[-120px]' src={doctor} alt="" />
             </div>
             <div className='flex-1'>
-                <h3 className='text-xl  text-white'>Appointment</h3>
+                <h3 className='text-xl  text-primary font-bold'>Appointment</h3>
                 <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corporis ut nihil molestias ipsam ratione minus quas quod molestiae commodi quae, optio unde neque dolorum veniam reprehenderit dolor perspiciatis sequi laboriosam assumenda accusamus dignissimos enim animi. Nemo adipisci non voluptatum?</p>
+                <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corporis ut nihil molestias ipsam ratione minus quas quod molestiae commodi quae, optio unde neque dolorum veniam reprehenderit dolor perspiciatis sequi laboriosam assumenda accusamus dignissimos enim animi. Nemo adipisci non voluptatum?</p>
                 <PrimaryBtn>Get Started</PrimaryBtn>
             </div>
         </section>
