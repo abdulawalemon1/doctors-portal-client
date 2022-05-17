c2658a7c183e678dfb1f50d3d7d53b8b8d0b32f1
abdulawalemon1 <emondx4@gmail.com>
2022-05-18 03:38:05 +0600

    data loaded from server

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,16 +1,19 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppointment = ({ date }) => {
-    const [services, setServices] = useState([])
+    const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
     useEffect(() => {
-        const url = `services.json`
+        const url = `http://localhost:5000/service`;
         fetch(url)
             .then(res => res.json())
             .then(data => setServices(data))
 
     }, [])
+
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
@@ -19,9 +22,14 @@ const AvailableAppointment = ({ date }) => {
                     services.map(service => <Service
                         key={service._id}
                         service={service}
+                        setTreatment={setTreatment}
                     ></Service>)
                 }
             </div>
+            {treatment && <BookingModal
+                date={date}
+                treatment={treatment}
+                setTreatment={setTreatment}></BookingModal>}
         </div>
     );
 };
