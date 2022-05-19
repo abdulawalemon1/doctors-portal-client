327eaf9b3db37da38adc2ef13c06274bf35c84ca
abdulawalemon1 <emondx4@gmail.com>
2022-05-19 20:30:27 +0600

    Login page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../.firebase.init';
+
+
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+
+    if (user) {
+        console.log(user);
+    }
+
     return (
-        <div>
-            <h1>This is login</h1>
+        <div className="hero min-h-screen ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className='text-center text-2xl font-bold'>Login</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <div className="divider">OR</div>
+                        <button
+                            onClick={() => signInWithGoogle()}
+                            className='btn btn-outline'>Continue with Google</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
